8f48dc822436465b1aa6e33e40c3ecc8f086128a
aldyvictor <aldyvictor1046@gmail.com>
2023-03-09 10:59:41 +0700

    Setup Tailwind Css

--- main-page/next.config.js
+++ main-page/next.config.js
@@ -1,7 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require('next-compose-plugins');
+const withCss = require('@zeit/next-css');
+const withReactSvg = require('next-react-svg');
+const withImages = require('next-images');
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = async () => {
+    return withPlugins([
+        [withCss, {}],
+        [
+            withReactSvg,
+            {
+                include: path.resolve(__dirname, './public/images'),
+                webpack(config, options) {
+                    return config;
+                },
+            },
+        ],
+        [withImages, {}],
+    ]);
+};

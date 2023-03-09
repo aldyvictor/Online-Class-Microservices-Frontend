be50feecc16f33fa68251f83ccb1b7a9af088999
aldyvictor <aldyvictor1046@gmail.com>
2023-03-09 12:06:41 +0700

    Setup React Membership Page

---
+++ membership-page/src/App.js
@@ -0,0 +1,11 @@
+import 'assets/css/style.css';
+
+function App() {
+    return (
+        <div className={'container mx-auto'}>
+            <h1 className={'text-6xl'}>React</h1>
+        </div>
+    );
+}
+
+export default App;
